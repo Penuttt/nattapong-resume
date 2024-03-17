447c582ac17653da9df94edc6245fae08d328074
Penuttt <nattapong.pe@mail.kmutt.ac.th>
2024-03-17 18:14:12 +0700

    modify-v4

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,8 +6,8 @@ class Social extends Component{
     
     <div className='social'>
     <a href="https://github.com/Penuttt" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
-    <a href="https://www.facebook.com/nattapong.panathiragul/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
-    <a href="https://github.com/Penuttt" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
+    <a href="https://www.instagram.com/penutt__?igsh=ZTV3d2o1MzlkN3lp&utm_source=qr" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+    <a href="https://www.facebook.com/nattapong.panathiragul/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
     </div>
 
 
